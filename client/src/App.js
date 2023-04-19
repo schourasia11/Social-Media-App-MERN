0deee54302b083985e0e3ffd694a16db9781c7a6
schourasia11 <schourasia@greenactionstudio.com>
2023-04-19 15:30:42 -0400

    start react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-// import { Container, AppBar, Typography } from '@material-ui/core';
-// import memories from './images/memories.png';
+import { Container, AppBar, Typography } from '@material-ui/core';
+import memories from './images/memories.png';
 
 function App() {
   return (
-    // <Container maxwidth="lg">
-    //   <AppBar position = "static" color = "inherit">
-    //     <Typography variant="h2" aligne="center">Memories</Typography>
-    //     <img src={memories} alt = "memories" height="60"></img>
-    //   </AppBar>
-    // </Container>
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar position = "static" color = "inherit">
+        <Typography variant="h2" aligne="center">Memories</Typography>
+        <img src={memories} alt = "memories" height="460"></img>
+      </AppBar>
+    </Container>
   );
 }
 
