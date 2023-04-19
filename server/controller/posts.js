596d896c6795c1c8b3cf003df0804c7cbbc33c3f
schourasia11 <schourasia@greenactionstudio.com>
2023-04-19 13:02:30 -0400

    added 2 routes for create and list all posts

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -1,3 +1,26 @@
-export const getPosts = (req,res) => {
-    res.send(`THIS WORKS`);
+import PostMessage from '../models/postMesage.js';
+
+export const getPosts = async (req,res) => {
+    try {
+        const postMessages = await PostMessage.find();
+        console.log(postMessages);
+
+        res.status(200).json(postMessages);
+    } catch(err) {
+        res.status(404).json({message: err.message});
+    }
+}
+
+export const createPost = async (req,res) => {
+    const body = req.body;
+
+    const newPost = new PostMessage(body);
+
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch (err) {
+        res.status(409).json({message: err.message});
+    }
 }
