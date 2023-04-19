3e118028edf4fe9da31ddbf20ab032fe1e8f6573
schourasia11 <schourasia@greenactionstudio.com>
2023-04-19 14:26:17 -0400

    files added for client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
+import React from 'react';
+// import { Container, AppBar, Typography } from '@material-ui/core';
+// import memories from './images/memories.png';
 
 function App() {
   return (
-    <div className="App">
+    // <Container maxwidth="lg">
+    //   <AppBar position = "static" color = "inherit">
+    //     <Typography variant="h2" aligne="center">Memories</Typography>
+    //     <img src={memories} alt = "memories" height="60"></img>
+    //   </AppBar>
+    // </Container>
+    <div>
       <h1>App</h1>
     </div>
   );
