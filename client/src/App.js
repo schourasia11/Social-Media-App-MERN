a35cec8c79a8853f9ec02b3cc633517866ffb638
schourasia11 <schourasia@greenactionstudio.com>
2023-04-20 10:58:31 -0400

    added for basic UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Container, AppBar, Typography } from '@material-ui/core';
+import { Container, AppBar, Typography, Grid, Grow } from '@material-ui/core';
 import memories from './images/memories.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 function App() {
   return (
@@ -9,6 +11,18 @@ function App() {
         <Typography variant="h2" aligne="center">Memories</Typography>
         <img src={memories} alt = "memories" height="460"></img>
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify ="space-between" alignItems='stretch' spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 }
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Post from './Post/Post';
+
+const Posts = () => {
+    return (
+        <>
+             <h1>POST</h1>
+             <Post />
+             <Post />
+        </>
+       
+    )
+}
+
+export default Posts;
