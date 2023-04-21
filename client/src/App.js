849ee43d5d020b86eef9a6483288e95f6312695c
schourasia11 <schourasia@greenactionstudio.com>
2023-04-21 10:53:23 -0400

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import { Container, AppBar, Typography, Grid, Grow } from '@material-ui/core';
 import memories from './images/memories.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import useStyles from './styles';
 
 function App() {
+
+  const classes = useStyles();
+
   return (
     <Container maxwidth="lg">
-      <AppBar position = "static" color = "inherit">
-        <Typography variant="h2" aligne="center">Memories</Typography>
-        <img src={memories} alt = "memories" height="460"></img>
+      <AppBar className = {classes.appBar} position = "static" color = "inherit">
+        <Typography className = {classes.heading} variant="h2" aligne="center">Memories</Typography>
+        <img className = {classes.appBar} src={memories} alt = "memories" height="60"></img>
       </AppBar>
       <Grow in>
         <Container>
